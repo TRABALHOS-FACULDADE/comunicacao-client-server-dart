7e7a4dc497f5f68b3ab61a6a24ee885b7a5af74d
Felipe Ribeiro <feliper.dev@gmail.com>
2024-05-06 22:57:40 -0300

    mais perguntas de jogos

--- bin/poll.dart
+++ bin/poll.dart
@@ -24,6 +24,23 @@ final poll = Poll(
           answer: '2013',
           answered: false,
         ),
+        Question(
+          question:
+              'Qual o nome do jogo de Pokémon onde o inicial é APENAS o Pikachu?',
+          answer: 'Yellow',
+          answered: false,
+        ),
+        Question(
+          question:
+              'Qual é o verdadeiro nome do personagem de roupa verde nos jogos da série "Zelda"?',
+          answer: 'Link',
+          answered: false,
+        ),
+        Question(
+          question: 'Qual console a Microsoft lançou no ano de 2013?',
+          answer: 'XBOX ONE',
+          answered: false,
+        ),
       ],
     ),
   ],
